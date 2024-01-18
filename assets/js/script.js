28ab2ab8950a01fa2270940ca2eb8f0ecb47e736
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-01-18 11:11:56 -0800

    second-commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-'use strict';
-
-
+'use strict'
 
 /**
  * add event on element
@@ -9,82 +7,78 @@
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
+      elem[i].addEventListener(type, callback)
     }
   } else {
-    elem.addEventListener(type, callback);
+    elem.addEventListener(type, callback)
   }
 }
-
-
 
 /**
  * navbar toggle
  */
 
-const navbar = document.querySelector("[data-navbar]");
-const navToggler = document.querySelector("[data-nav-toggler]");
-const navLinks = document.querySelectorAll("[data-nav-link]");
+const navbar = document.querySelector('[data-navbar]')
+const navToggler = document.querySelector('[data-nav-toggler]')
+const navLinks = document.querySelectorAll('[data-nav-link]')
 
-const toggleNavbar = () => navbar.classList.toggle("active");
+const toggleNavbar = () => navbar.classList.toggle('active')
 
-addEventOnElem(navToggler, "click", toggleNavbar);
+addEventOnElem(navToggler, 'click', toggleNavbar)
 
-const closeNavbar = () => navbar.classList.remove("active");
+const closeNavbar = () => navbar.classList.remove('active')
 
-addEventOnElem(navLinks, "click", closeNavbar);
-
-
+addEventOnElem(navLinks, 'click', closeNavbar)
 
 /**
  * header & back top btn active when scroll down to 100px
  */
 
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+const header = document.querySelector('[data-header]')
+const backTopBtn = document.querySelector('[data-back-top-btn]')
 
 const headerActive = function () {
   if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
+    header.classList.add('active')
+    backTopBtn.classList.add('active')
   } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
+    header.classList.remove('active')
+    backTopBtn.classList.remove('active')
   }
 }
 
-addEventOnElem(window, "scroll", headerActive);
-
-
+addEventOnElem(window, 'scroll', headerActive)
 
 /**
  * filter function
  */
 
-const filterBtns = document.querySelectorAll("[data-filter-btn]");
-const filterItems = document.querySelectorAll("[data-filter]");
+function showTab(tabName) {
+  const tabs = document.querySelectorAll('.our-services')
+  tabs.forEach((tab) => (tab.style.display = 'none'))
 
-let lastClickedFilterBtn = filterBtns[0];
+  const activeButton = document.querySelector('.filter-btn.active')
+  if (activeButton) {
+    activeButton.classList.remove('active')
+    activeButton.parentElement.style.backgroundColor = '' // Remove background color
+  }
 
-const filter = function () {
-  lastClickedFilterBtn.classList.remove("active");
-  this.classList.add("active");
-  lastClickedFilterBtn = this;
+  const selectedButton = document.querySelector(
+    `button[data-filter-btn="${tabName}"]`
+  )
+  if (selectedButton) {
+    selectedButton.classList.add('active')
+    selectedButton.parentElement.style.backgroundColor = 'black' // Set background color to gold
+  }
 
-  for (let i = 0; i < filterItems.length; i++) {
-    if (this.dataset.filterBtn === filterItems[i].dataset.filter ||
-      this.dataset.filterBtn === "all") {
-
-      filterItems[i].style.display = "block";
-      filterItems[i].classList.add("active");
-
-    } else {
-
-      filterItems[i].style.display = "none";
-      filterItems[i].classList.remove("active");
-
-    }
+  const selectedTab = document.querySelector(`.grid-list.${tabName}`)
+  if (selectedTab) {
+    selectedTab.style.display = 'block'
   }
 }
 
-addEventOnElem(filterBtns, "click", filter);
+
+// Show the "Our Mission" tab initially
+showTab('our-mission')
+
+addEventOnElem(filterBtns, 'click', filter)
