96468a1df3670a9104e0e88c06445306b4cc001b
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-01-19 06:43:34 -0800

    fix-tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,28 +52,54 @@ addEventOnElem(window, 'scroll', headerActive)
 /**
  * filter function
  */
-
-function showTab(tabName) {
-  const tabs = document.querySelectorAll('.our-services')
-  tabs.forEach((tab) => (tab.style.display = 'none'))
-
-  const activeButton = document.querySelector('.filter-btn.active')
-  if (activeButton) {
-    activeButton.classList.remove('active')
-    activeButton.parentElement.style.backgroundColor = '' // Remove background color
+function showTab(tabId) {
+  // Hide all tabs
+  var tabs = document.getElementsByClassName('tab-content')
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = 'none'
   }
 
-  const selectedButton = document.querySelector(
-    `button[data-filter-btn="${tabName}"]`
-  )
-  if (selectedButton) {
-    selectedButton.classList.add('active')
-    selectedButton.parentElement.style.backgroundColor = 'black' // Set background color to gold
+  // Remove 'active' class from all buttons with the class 'tab-btn'
+  var buttons = document.getElementsByClassName('tab-btn')
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('active')
+
+    // Remove 'white' class from btn-icon and btn-text
+    var btnIcon = buttons[i].querySelector('.btn-icon')
+    var btnText = buttons[i].querySelector('.btn-text')
+    if (btnIcon) {
+      btnIcon.classList.remove('active-icon')
+    }
+    if (btnText) {
+      btnText.classList.remove('active-text')
+    }
   }
 
-  const selectedTab = document.querySelector(`.grid-list.${tabName}`)
+  // Show the selected tab
+  var selectedTab = document.getElementById(tabId)
   if (selectedTab) {
     selectedTab.style.display = 'block'
+
+    // Add 'active' class to the corresponding tab-btn
+    var correspondingButton = document.querySelector(
+      '[onclick="showTab(\'' + tabId + '\')"]'
+    )
+    if (correspondingButton) {
+      correspondingButton.classList.add('active')
+
+      // Select elements with the classes 'btn-icon' and 'btn-text' from the corresponding tab-btn
+      var btnIcon = correspondingButton.querySelector('.btn-icon')
+      var btnText = correspondingButton.querySelector('.btn-text')
+
+      // Add 'white' class to the selected elements
+      if (btnIcon) {
+        btnIcon.classList.add('active-icon')
+      }
+
+      if (btnText) {
+        btnText.classList.add('active-text')
+      }
+    }
   }
 }
 
